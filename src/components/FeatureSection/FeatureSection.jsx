0f55745fa4165f294456770684e76c17956dc077
Ajay309 <ajaysing456.55@gmail.com>
2025-05-26 22:16:03 +0530

    changes

--- src/components/FeatureSection/FeatureSection.jsx
+++ src/components/FeatureSection/FeatureSection.jsx
@@ -13,7 +13,7 @@ export default function FeaturesSection() {
     <section className="features-section">
       <div className="container">
         <div className="row">
-          <div className="col-lg-8">
+          <div className="col-lg-12">
         <h5 className="heading-3  mb-5">
           Trusted Leading Trading Platforms
         </h5>

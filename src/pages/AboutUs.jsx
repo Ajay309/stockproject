7fbc3ffa10af910ae5fbcc54021fd328e3e4019d
nilancy <nilancyagarwal@gmail.com>
2025-06-25 17:00:34 +0530

    animation

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Footer from '../components/Footer/Footer'
 import AboutUsSection from '../components/AboutUsSection/AboutUsSection'
+import Animated from '../components/Animated.jsx';
 
 export default function ContactUs ()  {
   return (
     <>
     <div className="container-fluid py-5">
-      <AboutUsSection />
-        </div>
+      <Animated animation="fade-up" delay={100}>
+        <AboutUsSection />
+      </Animated>
+    </div>
     <Footer/>
     </>
   )

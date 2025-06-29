1467415c299b3782ae2abb395093d584b6178d07
nilancy <nilancyagarwal@gmail.com>
2025-06-29 23:05:28 +0530

    chnges

--- src/components/FeatureSection/FeatureSection.jsx
+++ src/components/FeatureSection/FeatureSection.jsx
@@ -21,13 +21,13 @@ export default function FeaturesSection() {
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
-            <h5 className="heading-3 mb-5 text-start">
+            <h5 className="heading-3 mb-2 text-start">
             <Animated animation="fade-up" delay={100}>Trusted Leading Trading Platforms</Animated>
             </h5>
           </div>
           <div className="logo-slider">
             <div className="slider-track">
-              {[...logos, ...logos].map((logo, index) => (
+              {[...logos, ...logos, ...logos, ...logos].map((logo, index) => (
                 <div className="slide" key={index}>
                   <img src={logo.image} alt={`Logo ${index + 1}`} />
                 </div>
